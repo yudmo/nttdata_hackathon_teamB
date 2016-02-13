cad58b5dee943fd43a7bacee5d4a2450b9e4d92b
Yudai MORIYA <s1520699@u.tsukuba.ac.jp>
2016-02-14 02:46:54 +0900

    summaryの取得時に，各ユーザのdepositの更新

--- app/Controller/SummariesController.php
+++ app/Controller/SummariesController.php
@@ -31,7 +31,7 @@ class SummariesController extends AppController{
     $params['fields'] = array(
       'Budget.user_id','Budget.date','SUM(Budget.money)','city.prefecture_id','city.id','user.type'
     );
-    $params['condtions'] = array('Budget.date BETWEEN ? AND ?'=> array($begin, $end));
+    $params['conditions'] = array('Budget.date BETWEEN ? AND ?'=> array($begin, $end));
 
     //cityとusertype毎のデータ取得
     $data = $this->Budget->find('all', $params);
@@ -45,7 +45,7 @@ class SummariesController extends AppController{
       $params['fields'] = array(
         '*'
       );
-      $params['condtions'] = array('Budget.date BETWEEN ? AND ?'=> array($begin, $end));
+      $params['conditions'] = array('Budget.date BETWEEN ? AND ?'=> array($begin, $end));
       $userNum = $this->Budget->find('all', $params);
       $value[0]['SUM(`Budget`.`money`)'] = (int)$value[0]['SUM(`Budget`.`money`)']/(int)$userNum;
       //savedataの成型
@@ -57,7 +57,59 @@ class SummariesController extends AppController{
       $temp['date'] = $value['Budget']['date'];
       array_push($savedata,$temp);
     }
-    $this->Summary->saveAll($savedata);
+    //$this->Summary->saveAll($savedata);
+
+    //各ユーザのdepositの計算
+    $users = $this->User->find('all');
+    foreach ($users as $key => $value) {
+      //summaryからaverage取得
+      $ave_params = array();
+      $ave_params['conditions'] = array(
+        'Summary.pref_id' => $value['User']['current_pref'],
+        'Summary.city_id' => $value['User']['current_city'],
+        'Summary.date' => $begin,
+        'Summary.type' => $value['User']['type']
+      );
+
+      $ave_params['fields'] = array(
+        'Summary.average'
+      );
+      $average = $this->Summary->find('first',$ave_params);
+      $average = $average['Summary']['average'];
+
+      //今月の食費取得
+      $params_user_budgets['fields'] = array(
+        'SUM(Budget.money)'
+      );
+      $params_user_budgets['conditions'] = array(
+        'Budget.date BETWEEN ? AND ?'=> array($begin, $end),
+        'Budget.user_id' => $value['User']['id'],
+      );
+
+      //cityとusertype毎のデータ取得
+      $monthly_budgets = $this->Budget->find('all', $params_user_budgets);
+      $monthly_budgets = $monthly_budgets[0][0]['SUM(`Budget`.`money`)'];
+      //差の導出,格納
+      $deposit = $average - $monthly_budgets;
+      $save_deposit = array();
+      $save_deposit['id'] = $value['User']['id'];
+      $save_deposit['deposit'] = $value['User']['deposit'] + $deposit;
+      $this->User->saveAll($save_deposit);
+    }
+
+  }
+
+  public function view(){
+    if(null == $this->Session->read('user')){
+      return $this->redirect(array('controller'=>'Users','action'=> 'login'));
+    }
+
+    $this->loadModel('User');
+    $this->loadModel('Budget');
+    $begin = date("Y-m-01");
+    $end = date("Y-m-d");
+
+
   }
 
 }
